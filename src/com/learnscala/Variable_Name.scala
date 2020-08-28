92ce38dcd556507534aa942bd4bd91ec4069a39d
Administrator <1047355811@qq.com>
2020-08-28 23:54:59 +0800

    添加变量名规范

---
+++ src/com/learnscala/Variable_Name.scala
@@ -0,0 +1,16 @@
+package com.learnscala
+
+object Variable_Name {
+  def main(args: Array[String]): Unit = {
+    //1、以字母或下划线开头，后接字母、数字、下划线
+    //2、以操作符开头。且只包含操作符
+    //3、用反引号包括任意字符串
+    val hello:String = ""
+    val hello2:String = ""
+    val _ab:String = ""
+    val Int:String = ""
+    val +-*/ :String = ""
+    val `if` :String = ""
+
+  }
+}
