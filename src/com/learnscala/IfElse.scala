eadbd0849d20ec3ff6f5d59137c144650b1f071a
Administrator <1047355811@qq.com>
2020-09-03 00:00:13 +0800

    添加ifelse

---
+++ src/com/learnscala/IfElse.scala
@@ -0,0 +1,15 @@
+package com.learnscala
+
+object IfElse {
+  def main(args: Array[String]): Unit = {
+    if(true){
+      return 0
+    }
+    else if(false){
+      return 1
+    }
+    else {
+      return 2
+    }
+  }
+}
