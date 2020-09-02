76b19a1065b84169a4381fa298dd2004fa9d16b3
Administrator <1047355811@qq.com>
2020-09-03 00:10:09 +0800

    添加ifelse

--- src/com/learnscala/IfElse.scala
+++ src/com/learnscala/IfElse.scala
@@ -2,14 +2,29 @@ package com.learnscala
 
 object IfElse {
   def main(args: Array[String]): Unit = {
+    //单分支
     if(true){
-      return 0
+      1
+    }
+    //多分支
+    if(true){
+      1
+    } else {
+      2
+    }
+    //if可以有返回值
+    val res = if(true){
+       0
     }
     else if(false){
-      return 1
+       1
     }
     else {
-      return 2
+       2
     }
+    //三元运算符
+    var b:Boolean = true
+    var res1 = if(b) true else false
+    //scala没有switch此处不提
   }
 }
