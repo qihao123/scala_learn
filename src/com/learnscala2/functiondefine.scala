b891937798b6ef5731e7dd28875fe4c6203d69b9
Administrator <1047355811@qq.com>
2020-09-07 23:22:35 +0800

    添加scala函数定义

---
+++ src/com/learnscala2/functiondefine.scala
@@ -0,0 +1,35 @@
+package com.learnscala2
+
+object functiondefine {
+  def main(args: Array[String]): Unit = {
+
+  }//函数定义规则
+    /*
+    函数1：无参数，无返回值
+    函数2：无参数，有返回值
+    函数3：有参数，无返回值
+    函数4：有参数，有返回值
+    函数5：多参数，无返回值
+    函数6：多参数，有返回值
+     */
+    def f1() : Unit={
+
+    }
+    def f2() : Int={
+      1
+    }
+    def f3(x:Int) : Unit = {
+
+    }
+    def f4(x:Int) : Int = {
+      x
+    }
+    def f5(x:Int,y:Int) : Unit = {
+
+    }
+    def f6(x:Int,y:Int) : Int = {
+      x+y
+    }
+
+
+}
